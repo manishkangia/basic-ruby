1120e52eaafc50913962b87adaa42414be9433a4
ShubhamGupta <shubham.gupta@vinsol.com>
2013-01-12 16:56:29 +0530

    first; \c

--- add_time.rb
+++ add_time.rb
@@ -4,7 +4,7 @@ def add_time time_1, time_2
   sum_time[1] = time1[1].to_i + time2[1].to_i
   sum_time[2] = time1[2].to_i + time2[2].to_i
   if sum_time[2] > 60 then sum_time[1] += sum_time[2]/60; sum_time[2] = sum_time[2]%60 end
-  if sum_time[1] > 60 then sum_time[0] += sum_time[1]/60; sum_time[1] = sum_time[2]%60 end
+  if sum_time[1] > 60 then sum_time[0] += sum_time[1]/60; sum_time[1] = sum_time[1]%60 end
   if sum_time[0] >= 24 then print "#{sum_time[0]/24} day(s) & " ; sum_time[0] %= 24; end
   puts "#{sum_time[0]}:#{sum_time[1]}:#{sum_time[2]}"
 end
