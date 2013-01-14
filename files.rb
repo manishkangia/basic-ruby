1ebee15827cca3b9e748b87631cac47b9e2d0c62
ShubhamGupta <shubham.gupta@vinsol.com>
2013-01-14 14:44:42 +0530

    first

---
+++ files.rb
@@ -0,0 +1,9 @@
+employee_record = Hash.new('')
+IO.foreach("employee.csv") do |line| 
+  details = line.split(',')
+  employee_record [details[2].strip] += "#{details[0]}(EmpId: details[1])\n"
+end
+
+file_stream = IO.sysopen("employee_record.csv", "w")
+a = IO.new(file_stream, "a")
+employee_record.each { |key, value| a.write(key + ":\n" + value) }
