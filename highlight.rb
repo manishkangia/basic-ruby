64892b5fd8cbf65b2f5ca9985f517aa1ee023303
ShubhamGupta <shubham.gupta@vinsol.com>
2013-01-16 15:10:12 +0530

    third

--- highlight.rb
+++ highlight.rb
@@ -1,4 +1,14 @@
+def search input_string, search_item
+  can_count = 0
+  puts input_string.gsub( /#{search_item}/i ){ |s| "(#{s})" }
+  input_string.scan( /#{search_item}/i ) { can_count += 1 }
+  puts "Total Occurences Found : #{can_count}"
+end
+
 puts 'Please enter a string to highlight and count the occurences of the word "can" '
-can_count = 0
-puts gets.gsub( /can/ , '(Can)' ).scan( /(Can)/ ) { can_count += 1 }
-puts "Total Occurences Found : #{can_count}"
+search_string = gets.chomp
+
+puts 'search for'
+search_for = gets.chomp
+
+search( search_string, search_for )
