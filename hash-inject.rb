a9546e74140df0c004e6a0e0a778b603232ac0ad
ShubhamGupta <shubham.gupta@vinsol.com>
2013-01-16 12:27:47 +0530

    second

--- hash-inject.rb
+++ hash-inject.rb
@@ -1,25 +1,28 @@
-collection = ['abc','def',1234,234,'abcd','x','mnop',5,'zZzZ']
-def create_hash_by_length collection
-  store_by_length = Hash.new()
-  for str in collection
-    if store_by_length.keys.include? (str.to_s.length)
-      store_by_length[str.to_s.length] << str
-    else
-      store_by_length[str.to_s.length] = []
-      store_by_length[str.to_s.length] << str
+class Array
+  def get_length_hash
+    store_by_length = Hash.new()
+    for str in self
+      if store_by_length.keys.include? (str.to_s.length)
+        store_by_length[str.to_s.length] << str
+        else
+          store_by_length[str.to_s.length] = []
+          store_by_length[str.to_s.length] << str
+      end
     end
+  return store_by_length
   end
-  store_by_length
-end
 
-def even_odd collection_hash
-  save_even_odd = Hash.new()
-  save_even_odd["even"],save_even_odd["odd"] = [],[]
-  collection_hash.inject(save_even_odd) do |result,hash_value| 
-    if hash_value[0].to_i.odd? then result["odd"] << hash_value[1] 
-    elsif hash_value[0].to_i.even? then result["even"] << hash_value[1]
+  def even_odd 
+    collection_hash = get_length_hash 
+    save_even_odd = Hash.new()
+    save_even_odd["even"],save_even_odd["odd"] = [],[]
+    collection_hash.inject(save_even_odd) do |result,hash_value| 
+      if hash_value[0].to_i.odd? then result["odd"] << hash_value[1] 
+        elsif hash_value[0].to_i.even? then result["even"] << hash_value[1]
+      end
+      result
     end
-    result
   end
 end
-puts even_odd (create_hash_by_length collection)
+result_hash = ['abc','def',1234,234,'abcd','x','mnop',5,'zZzZ', 4586].even_odd
+puts result_hash
