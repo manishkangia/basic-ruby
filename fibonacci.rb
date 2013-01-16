c4b652a469e540507f127435068099e47b34d213
ShubhamGupta <shubham.gupta@vinsol.com>
2013-01-16 10:46:38 +0530

    second

--- fibonacci.rb
+++ fibonacci.rb
@@ -4,7 +4,7 @@ def fib max_value
   while sum < max_value
     result_array = yield first_num,sum
     sum, first_num = result_array[0], result_array[1]
-    print " #{sum}"
+    print " #{sum}" if sum < max_value
   end 
 end
 fib ( 1000 ) { |a,b| [a + b, b] }
