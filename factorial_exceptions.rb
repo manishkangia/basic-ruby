3c416fe2d2f85da82df6905715fb40f375b53e1d
ShubhamGupta <shubham.gupta@vinsol.com>
2013-01-16 15:32:16 +0530

    rescued

--- factorial_exceptions.rb
+++ factorial_exceptions.rb
@@ -1,6 +1,8 @@
 def factorial num
   raise ArgumentError, 'Factorial for a negative number!!' if num < 0
   (1..num).inject(1) { |result,n| result = result*n }
+  rescue ArgumentError
+  puts 'the number is negative'
 end
 puts 'Enter a number to get its factorial'
 puts factorial gets.chop.to_i
