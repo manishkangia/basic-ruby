5895407b840253722f30a7fb108714f6649285f8
ShubhamGupta <shubham.gupta@vinsol.com>
2013-01-16 11:26:35 +0530

    second

--- customer_accounts.rb
+++ customer_accounts.rb
@@ -3,7 +3,7 @@ class Customers
   attr_accessor :name, :account_no, :balance
   def initialize ( cust_name )
     @name = cust_name
-    @account_no = @@customer_total
+    @account_no = @@customer_total + 1
     @@customer_total += 1
     @balance = 1000
   end
@@ -11,22 +11,22 @@ class Customers
     @balance += amount if amount > 0
   end
   def withdrawl amount
-    if ( @balance - amount )> 0 && amount > 0 then @balance -= amount
-    elsif ( @balance - amount )< 0 && amount > 0 then puts 'insufficient balance for such a withdrawl, action aborted'
-    else puts 'inappropriate quatity' 
+    if ( @balance - amount ) > 0 && amount > 0 then @balance -= amount
+      elsif ( @balance - amount ) < 0 then puts 'insufficient balance for such a withdrawl, action aborted'
+        else puts 'inappropriate quatity' 
     end
   end
 end
 new_customer = Customers.new ('smith')
 puts new_customer.inspect
 
-new_customer.deposit 1000 
+new_customer.deposit( 1000 )
 puts new_customer.inspect
 
-new_customer.withdrawl -500 #bad input
+new_customer.withdrawl( -500 )#bad input
 puts new_customer.inspect
 
-new_customer.withdrawl 2500 #excess withdrawl
+new_customer.withdrawl( 2500 )#excess withdrawl
 puts new_customer.inspect
 
 new_customer = Customers.new ('Bob')
