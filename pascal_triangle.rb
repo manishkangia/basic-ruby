bc837d099a2892f1f6ed287706bb9541b8d58246
ShubhamGupta <shubham.gupta@vinsol.com>
2013-01-16 12:20:51 +0530

    second

--- pascal_triangle.rb
+++ pascal_triangle.rb
@@ -1,15 +1,16 @@
 def generate_pascal_triangle rows
   puts "1\n1 1\n"
-  first_row = [1,1]
-  begin 
-    next_row = yield first_row 
-    next_row.each { |num| print "#{num} " }
-    first_row = next_row
+  row = [1,1]
+  begin
+    row = yield row 
     puts
-  end while next_row.length <= ( rows )
+  end while row.length <= ( rows )
 end
-generate_pascal_triangle (6) do |last_row| 
+generate_pascal_triangle (6) do |last_row|
   new_row = [1]
-  last_row.each_index { |value_at| new_row <<( last_row[value_at] + last_row[value_at + 1] ) if value_at < last_row.length - 1}
+  last_row.each_index do |value_at| 
+    new_row << ( last_row[value_at] + last_row[value_at + 1] ) if value_at < last_row.length - 1 
+  end
   new_row << 1
+  new_row.each { |value| print "#{value} " }
 end
