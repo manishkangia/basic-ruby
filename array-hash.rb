166086088e37d6a59275c6d77adc1be2ce2cf504
ShubhamGupta <shubham.gupta@vinsol.com>
2013-01-16 13:51:19 +0530

    second

--- array-hash.rb
+++ array-hash.rb
@@ -1,11 +1,16 @@
-collection = ['abc','def',1234,234,'abcd','x','mnop',5,'zZzZ', 4586]
-store_by_length = Hash.new()
-for str in collection
-  if store_by_length.keys.include? (str.to_s.length)
-    store_by_length[str.to_s.length] << str
-  else
-    store_by_length[str.to_s.length] = []
-    store_by_length[str.to_s.length] << str
+class Array
+  def length_hash
+    store_by_length = Hash.new()
+    for str in self
+      if store_by_length.keys.include? (str.to_s.length)
+        store_by_length[str.to_s.length] << str
+        else
+          store_by_length[str.to_s.length] = []
+          store_by_length[str.to_s.length] << str
+      end
+    end
+  return store_by_length
   end
 end
-puts store_by_length
+result_hash = ['abc','def',1234,234,'abcd','x','mnop',5,'zZzZ', 4586].length_hash
+puts result_hash
