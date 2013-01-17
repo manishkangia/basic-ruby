df73623d1f7900781e9105de0ff018829c0b43e2
ShubhamGupta <shubham.gupta@vinsol.com>
2013-01-17 15:01:46 +0530

    check

--- add_time.rb
+++ add_time.rb
@@ -1,19 +1,22 @@
-def add_time time1, time2
-  time1 = time1.map { |num| num.to_i }
-  time2 = time2.map { |num| num.to_i }
-  sum_time = []
-  sum_time[0] = time1[0] + time2[0]
-  sum_time[1] = time1[1] + time2[1]
-  sum_time[2] = time1[2] + time2[2]
-  if sum_time[2] > 60 then sum_time[1] += sum_time[2]/60; sum_time[2] = sum_time[2]%60 end
-  if sum_time[1] > 60 then sum_time[0] += sum_time[1]/60; sum_time[1] = sum_time[1]%60 end
-  if sum_time[0] >= 24 then print "#{sum_time[0]/24} day(s) & " ; sum_time[0] %= 24; end
-  puts "#{sum_time[0]}:#{sum_time[1]}:#{sum_time[2]}"
+require 'time'
+def add_time time_1, time_2
+  valid_regex = /^(([0-1][0-9])|([2][0-3])|([0-9])):[0-5]?[0-9]:[0-5]?[0-9]$/
+  if ( (time_1 =~ valid_regex) && (time_2 =~ valid_regex ) ) then
+    t1 = Time.strptime(time_1,'%H:%M:%S')
+    t2 = Time.strptime(time_2,'%H:%M:%S')
+    sum_time = t1 + t2.sec + t2.min * 60 + t2.hour * 3600
+    sum_time = (sum_time - Time.strptime('0:0:0','%H:%M:%S')).to_i
+    days = sum_time/86400
+    sum_time = sum_time%86400
+    hours = sum_time/3600
+    sum_time = sum_time%3600
+    mins = sum_time/60
+    secs = sum_time%60
+    puts "#{days} day(s) & #{hours}:#{mins}:#{secs}"
+    else puts 'invalid time values'
+  end
 end
-
 puts 'enter two times to add them'
 time_1 = gets.chomp
 time_2 = gets.chomp
-if ( (time_1 =~ /^(([0-1][0-9])|([2][0-3])|([0-9])):[0-5]?[0-9]:[0-5]?[0-9]$/) && (time_2 =~ /^(([0-1][0-9])|([2][0-3])|([0-9])):[0-5]?[0-9]:[0-5]?[0-9]$/ ) ) then add_time time_1.split(':'), time_2.split(':')
-  else puts 'invalid time values'
-end
+add_time time_1, time_2
