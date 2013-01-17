fc777ed2dd1fb9d9ba0637c8a4abfe9deea56b68
ShubhamGupta <shubham.gupta@vinsol.com>
2013-01-17 17:59:16 +0530

    fixed

--- add_time.rb
+++ add_time.rb
@@ -1,7 +1,7 @@
 require 'time'
 def add_time time_1, time_2
   valid_regex = /^(([0-1][0-9])|([2][0-3])|([0-9])):[0-5]?[0-9]:[0-5]?[0-9]$/
-  if ( (time_1 =~ valid_regex) && (time_2 =~ valid_regex ) ) then
+  if ( (time_1 =~ valid_regex) && (time_2 =~ valid_regex ) )
     t1 = Time.strptime(time_1,'%H:%M:%S')
     t2 = Time.strptime(time_2,'%H:%M:%S')
     sum_time = t1 + t2.sec + t2.min * 60 + t2.hour * 3600
@@ -16,6 +16,7 @@ def add_time time_1, time_2
     else puts 'invalid time values'
   end
 end
+
 puts 'enter two times to add them'
 time_1 = gets.chomp
 time_2 = gets.chomp
