eaacd7846a117e0ae0c0642e268ad78f917761d8
ShubhamGupta <shubham.gupta@vinsol.com>
2013-01-18 12:22:27 +0530

    fixed

--- add_time_any.rb
+++ add_time_any.rb
@@ -2,23 +2,22 @@ require 'time'
 def add_time *time_array
   all_valid = validate_time time_array
   if all_valid 
-    sum = [0,0,0]
+    sum = 0
     time_array.each do |time1|
       t1 = Time.strptime(time1,'%H:%M:%S') 
-      sum[0] += t1.hour
-      sum[1] += t1.min
-      sum[2] += t1.sec 
+      sum += t1.hour*3600 + t1.min*60 + t1.sec 
     end
-    sum = sum[0] * 3600 + sum[1] * 60 + sum[2]
-    days = sum/86400
-    sum = sum%86400
-    hours = sum/3600
-    sum = sum%3600
-    mins = sum/60
+    days,sum = div_func sum,86400
+    hours,sum = div_func sum,3600
+    mins,sum = div_func sum,60
     secs = sum%60
     puts "The sum is : #{days} day(s) & #{hours}:#{mins}:#{secs}"
   else puts 'invalid time values'
   end
+end
+
+def div_func value, div_by
+  return value/div_by, value % div_by
 end
 
 def validate_time time_array
