aa72934f2f08fa7aa10540532eb5852a8207b159
ShubhamGupta <shubham.gupta@vinsol.com>
2013-01-18 11:32:39 +0530

    fixed

--- add_time_any.rb
+++ add_time_any.rb
@@ -1,15 +1,6 @@
 require 'time'
 def add_time *time_array
-  all_valid = true
-  valid_regex = /^(([0-1][0-9])|([2][0-3])|([0-9])):[0-5]?[0-9]:[0-5]?[0-9]$/
-  time_array.each do |time1|
-    if time1 =~ valid_regex then time1 = time1.split(':')
-    else 
-      all_valid = false
-      puts 'invalid input'
-      return
-    end
-  end
+  all_valid = validate_time time_array
   if all_valid 
     sum = [0,0,0]
     time_array.each do |time1|
@@ -25,7 +16,14 @@ def add_time *time_array
     sum = sum%3600
     mins = sum/60
     secs = sum%60
-    puts "#{days} day(s) & #{hours}:#{mins}:#{secs}"
+    puts "The sum is : #{days} day(s) & #{hours}:#{mins}:#{secs}"
+  else puts 'invalid time values'
   end
+end
+
+def validate_time time_array
+  valid_regex = /^(([0-1][0-9])|([2][0-3])|([0-9])):[0-5]?[0-9]:[0-5]?[0-9]$/
+  time_array.each { |time1| return false unless time1 =~ valid_regex }
+  return true
 end
 add_time "11:23:07","22:53:45","0:23:23","23:45:56"
